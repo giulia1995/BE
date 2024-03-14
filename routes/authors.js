2cd4cf31842500fb8f3a7391eb17088e61136d39
Giulia_vi95 <giulia.vaccaro95@gmail.com>
2024-03-14 19:03:31 +0100

    finish routes

--- routes/authors.js
+++ routes/authors.js
@@ -18,6 +18,72 @@ router.get("/getAuthors", async ( request, response)=>{
     }
 })
 
+router.get("/getAuthors/:id", async (request, response)=>{
+  const {id} = request.params;
+  try{
+   const author = await AuthorsModel.findById(id);
+   if (!author){
+    return response.status(404)
+    .send({
+      statusCode:404,
+      message:"The requested author does not exist!"
+    })
+   }
+   response.status(200)
+   .send(author)
+  }catch(e){
+   response.status(500)
+   .send({
+    statusCode:500,
+    message:"Internal Server Error"
+   })
+  }
+})
+router.get("/getAuthor/byName/:query", async (request, response)=>{
+  const {query} = request.params;
+  try{
+    const author = await AuthorsModel.find({
+      firstName:{
+        $regex: ".*" + query + ".*",
+        $options: "i",
+      }
+    })
+    if(!author){
+      return response.status(404)
+      .send({
+        statusCode:404,
+        message: "Author not found with the given query"
+      })
+    }
+    response.status(200)
+    .send(author);
+  }catch(e){
+    response.status(500)
+   .send({
+    statusCode:500,
+    message:"Internal Server Error"
+   })
+  }
+})
+router.get("/getAuthor/byAge/:age", async (request, response)=>{
+  const {age} = request.params;
+  try{
+   const authorByAge = await  AuthorsModel.find({
+    age: {
+      $gte: age,
+      
+    }
+   })
+   response.status(200).send(authorByAge)
+  }catch(e){
+    response.status(500)
+    .send({
+     statusCode:500,
+     message:"Internal Server Error"
+    })
+  }
+})
+
 router.post("/createAuthor", async (request, response )=>{
     const newAuhtor = new AuthorsModel({
        firstName:request.body.firstName,
@@ -43,6 +109,54 @@ router.post("/createAuthor", async (request, response )=>{
       } )
     }
 })
+router.patch("/updateAuthor/:id", async (request, response)=>{
+  const {id} = request.params
+  try{
+    const author = await AuthorsModel.findById(id);
+    if(!author){
+       return response
+       .status(404)
+       .send({
+        statusCode:404,
+        message: "The requested author not exist!"
+       })
+    }
+    const updatedData = request.body;
+    const options = {new: true};
 
+    const result = await AuthorsModel.findByIdAndUpdate(id, updatedData, options);
+    response
+           .status(200)
+           .send(result)
+
+  }catch(e){
+    response
+      .status(500)
+      .send({
+        statusCode:500,
+        message: "Internal server error"
+      })
+  }
+})
+
+router.delete("/deleteAuthor/:id", async (request, response)=>{
+  const {id} = request.params;
+  try{
+       const author = await AuthorsModel.findByIdAndDelete(id);
+       if (!author){
+        return response
+       .status(404)
+       .send({
+        statusCode:404,
+        message: "The requested author not exist!"
+       })
+       }
+       response.status(200)
+       .send("Author with id ${id} successfully removed")
+
+  }catch(e){
+
+  }
+})
 
 module.exports = router;
